59c59787d150b7b72ce98225b1c722d8a902ba18
Justin Darmawan <jdarmawan.jd@gmail.com>
2024-03-12 15:33:53 +0700

    feat: convert

--- internal/controller/convert_controller.go
+++ internal/controller/convert_controller.go
@@ -2,7 +2,10 @@ package controller
 
 import (
 	"go-img-processing/bootstrap"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"gocv.io/x/gocv"
@@ -16,51 +19,62 @@ func NewConvertController(server *gin.Engine, config *bootstrap.Container) {
 }
 
 func Convert(ctx *gin.Context) {
-	form, err := ctx.MultipartForm()
+	file, err := ctx.FormFile("image")
 	if err != nil {
-		ctx.JSON(400, gin.H{"error": "Failed to parse form-data"})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Failed to get image from form-data"})
 		return
 	}
 
-	file := form.File["image"]
-	if len(file) != 1 {
-		ctx.JSON(400, gin.H{"error": "Invalid file upload"})
+	if !strings.HasSuffix(strings.ToLower(file.Filename), ".png") {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Only PNG files are accepted"})
 		return
 	}
 
-	src, err := file[0].Open()
+	src, err := file.Open()
 	if err != nil {
-		ctx.JSON(500, gin.H{"error": "Failed to open file"})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open file"})
 		return
 	}
 	defer src.Close()
 
-	// Read the uploaded image using GoCV
-	inputImage := gocv.IMRead(file[0].Filename, gocv.IMReadColor)
+	dst, err := os.CreateTemp("", "uploaded-*.png")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create temporary file"})
+		return
+	}
+	defer func() {
+		dst.Close()
+		os.Remove(dst.Name())
+	}()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to copy file contents"})
+		return
+	}
+
+	inputImage := gocv.IMRead(dst.Name(), gocv.IMReadColor)
 	if inputImage.Empty() {
-		ctx.JSON(500, gin.H{"error": "Failed to read uploaded image"})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read uploaded image"})
 		return
 	}
 	defer inputImage.Close()
 
-	// Convert PNG image to JPEG image
-	outputImage := gocv.NewMat()
-	defer outputImage.Close()
-
-	// Write the converted image to a temporary file
 	tempFile, err := os.CreateTemp("", "output-*.jpg")
 	if err != nil {
-		ctx.JSON(500, gin.H{"error": "Failed to create temporary file"})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create temporary file"})
 		return
 	}
-	defer tempFile.Close()
+	defer func() {
+		dst.Close()
+		os.Remove(tempFile.Name())
+	}()
 
-	// Write the output image to the temporary file
-	if ok := gocv.IMWrite(tempFile.Name(), outputImage); !ok {
-		ctx.JSON(500, gin.H{"error": "Failed to write output image"})
+	if ok := gocv.IMWrite(tempFile.Name(), inputImage); !ok {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to write output image"})
 		return
 	}
 
-	// Return the path to the converted image
-	ctx.JSON(200, gin.H{"message": "Image converted successfully", "path": tempFile.Name()})
+	ctx.Header("Content-Disposition", "attachment; filename="+strings.Split(file.Filename, ".")[0]+".jpg")
+	ctx.File(tempFile.Name())
 }
